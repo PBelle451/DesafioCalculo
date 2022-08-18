7a83a3c45cfd1e4137747521c903076fd0ccfc6d
Pedro Belle <99338512+PBelle451@users.noreply.github.com>
2022-08-18 15:01:50 -0300

    Add files via upload

--- calculo.java
+++ calculo.java
@@ -5,11 +5,11 @@ public class calculo {
     public static int somaDigitos(int numero){
         int soma = 0;
         while(numero>0){
-            soma += numero % 10;
+            soma += numero % 10; //Realiza o somatório dos algarismos, a variável soma le-rá um dígito inserido pelo usuário dividindo por 10 e utilizando o resto da divisão e assim sucessivamente com outros algarismos do inteiro inserido
             numero /= 10;
         }
         return soma;
-    }
+    } //Cria o método para realizar a soma dos algarismos
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,23 +17,23 @@ public class calculo {
         int S = scanner.nextInt();
         if (S < 1 || S > 36){
             System.out.println("Erro. S deve ser maior ou igual a 1 e menor ou igual a 36");
-        }
+        } //Lê o valor de S e verifica se está de acordo com as condições do If
         System.out.println("Insira o valor de A: ");
         int A = scanner.nextInt();
         if (A < 1 || A > 10000){
             System.out.println("Erro. A deve ser maior ou igual a 1 e menor ou igual a 10000");
-        }
+        } //Lê o valor de A e verifica se está de acordo com as condições do If
         System.out.println("Insira o valor de B: ");
         int B = scanner.nextInt();
         if (B < 1 || B > 10000){
             System.out.println("Erro. B deve ser maior ou igual a 1 e menor ou igual a 10000");
-        }
+        } //Lê o valor de B e verifica se está de acordo com as condições do If
         int qtd = 0;
 
         for(int i = A; i <= B; i++){
             if(somaDigitos(i) == S) {
                 qtd++;
-            }
+            } // Chama de volta o método criado acima para executar a soma dos algarismos que são iguais ao número S digitado pelo usuário
         }
         System.out.println(qtd);
     }
